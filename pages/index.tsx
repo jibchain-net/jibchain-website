bb2ae51e0b5957d7db12393f70b9a828761f627e
Satoshi Okamoto <tom@duckyscan.com>
2024-01-15 16:46:57 +0700

    init landing page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+
+
+const Home: NextPage = () => {
+  return (
+    <h1>JibCHain</h1>
+  );
+};
+
+export default Home;
