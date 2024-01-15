2962d516fb526e91c781c6e799311e98c2e3c9f0
Satoshi Okamoto <tom@duckyscan.com>
2024-01-16 02:31:54 +0700

    Add states

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next';
 import Hero from '../components/Hero';
+import States from '../components/States';
 
 
 const Home: NextPage = () => {
   return (
-    <Hero />
+    <><Hero /><States /></>
   );
 };
 
