1be3ca861ae818f8fe3bc24e77fd859d99ed508e
Satoshi Okamoto <tom@duckyscan.com>
2024-01-19 00:37:30 +0700

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -57,6 +57,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="twitter:description" content="JIB Chain, an EVM-based platform, is powered by a community of developers and secured by at-home stakers globally. It offers familiar tooling and is soon introducing trustless bridges to major blockchains, ensuring a seamless and secure experience." />
         <meta name="twitter:image" content={coverImage.src} />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#13254d" />
+        <meta name="msapplication-TileColor" content="#13254d" />
+        <meta name="theme-color" content="#13254d"></meta>
+
       </Head>
         <Navbar />
         <Component {...pageProps} />
