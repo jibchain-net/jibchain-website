7cdf0e5cbb2aff80bbd398271421975b595588a1
Dome C <dome@tel.co.th>
2024-01-19 00:41:00 +0700

    fix next config for static build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+
   reactStrictMode: true,
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
