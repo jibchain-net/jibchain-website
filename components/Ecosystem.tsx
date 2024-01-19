10f3a4e56b9792629e75ad0ad319ab893c4a65ce
Satoshi Okamoto <tom@duckyscan.com>
2024-01-20 00:01:59 +0700

    Add link in ecosystem

--- components/Ecosystem.tsx
+++ components/Ecosystem.tsx
@@ -7,12 +7,14 @@ import iconJibpunk from '../public/img/icon/ecosystem/jibpunk.png';
 import iconCommudao from '../public/img/icon/ecosystem/commudao.png';
 import iconMeawneon from '../public/img/icon/ecosystem/meawneon.png';
 import iconJibswap from '../public/img/icon/ecosystem/jibswap.png';
+import Link from 'next/link';
 
 interface EcosystemItem {
   icon: StaticImageData;
   name: string;
   tag: string[];
   description: string;
+  link: string;
 }
 
 const items: EcosystemItem[] = [
@@ -21,38 +23,44 @@ const items: EcosystemItem[] = [
     name: 'JibSwap',
     tag: ['DEFI'],
     description: 'JIBSWAP | Swap on the leading decentralized crypto trading protocol',
+    link: 'https://jibswap.com/#/swap',
   },
   {
     icon: iconJibid,
     name: 'JibID',
     tag: ['NFT'],
     description: 'Your Passport to Web3 Simplicity',
+    link: 'https://www.jibid.xyz/',
   },
   {
     icon: iconJibpunk,
     name: 'Jibpunk',
     tag: ['DEFI', 'NFT'],
     description: 'A randomly generated JIB Punk representing a unique individual in the community.',
+    link: 'https://www.jibpunk.xyz/',
   },
   {
     icon: iconCommudao,
     name: 'Commudao',
     tag: ['DEFI', 'NFT'],
     description: 'Web3 protocol providing On-chain gamification and Dapp services. Developed by CMDAO LABS.',
+    link: 'https://commudao.xyz/',
   },
   {
     icon: iconMeawneon,
     name: 'Meawneon',
     tag: ['NFT'],
     description: 'MeowNeon is the first NFTs (ERC-1155) in JIBCHAIN L1 and largest marketplace for NFTs and crypto collectibles.',
+    link: 'https://meowneon.app/',
   },
   {
     icon: iconJiblend,
     name: 'JIBLEND',
     tag: ['DEFI'],
     description: 'Unleash the Power of Your Crypto',
+    link: 'https://jiblend.xyz/',
   },
-  
+
 ];
 
 export default function Ecosystem() {
@@ -76,12 +84,19 @@ export default function Ecosystem() {
                 <div>
                   <div className="grid grid-cols-1 justify-items-start">
                     <span className='pl-3'>
-                      <Image
-                        src={item.icon}
-                        width={72}
-                        height={72}
-                        alt={item.name}
-                      />
+                      <Link
+                        href={item.link}
+                        rel="noopener noreferrer" 
+                        target="_blank"
+                      >
+                        <Image
+                          src={item.icon}
+                          width={72}
+                          height={72}
+                          alt={item.name}
+                        />
+                      </Link>
+
                     </span>
                     <div className='text-lg font-semi-bold p-3'>
                       <span className='text-white text-2xl'>{item.name}</span>
