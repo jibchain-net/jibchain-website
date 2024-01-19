815843910cc3653186abb3bdc97473334c1bc37c
Satoshi Okamoto <tom@duckyscan.com>
2024-01-19 12:51:49 +0700

    Add link in partner

--- components/Partner.tsx
+++ components/Partner.tsx
@@ -5,28 +5,34 @@ import iconJib from '../public/img/icon/partner/jib.png'
 import iconThaichain from '../public/img/icon/partner/thaichain.png'
 import iconSupra from '../public/img/icon/partner/supra.png'
 import iconCm from '../public/img/icon/partner/cm.png'
+import Link from 'next/link'
 
 
 const items = [
   {
     icon: iconTokennine,
-    alt: 'Tokennine'
+    alt: 'Tokennine',
+    link: 'https://tokenine.co/',
   },
   {
     icon: iconJib,
-    alt: 'Jib'
+    alt: 'Jib',
+    link: 'https://www.jib.co.th/web/',
   },
   {
     icon: iconThaichain,
-    alt: 'Thaichain'
+    alt: 'Thaichain',
+    link: 'https://www.thaichain.io/',
   },
   {
     icon: iconSupra,
-    alt: 'Supra'
+    alt: 'Supra',
+    link: 'https://supraoracles.com/',
   },
   {
     icon: iconCm,
-    alt: 'Cm'
+    alt: 'Cm',
+    link: '/',
   },
   
   
@@ -50,14 +56,23 @@ export default function Partner() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             {items.map((item) => (
-              <Image
-                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                src={item.icon}
-                alt={item.alt}
-                width={158}
-                height={48}
-                key={item.alt}
-              />
+              <div key={item.alt}>
+              <Link 
+                href={item.link}
+                rel="noopener noreferrer" 
+                target="_blank"
+                
+              >
+                <Image
+                  className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                  src={item.icon}
+                  alt={item.alt}
+                  width={158}
+                  height={48}
+                  
+                />
+              </Link>
+              </div>
             ))}
           </div>
         </div>
