a1ebc446de816901e20cb9dca1a83dfb4bcb9553
Satoshi Okamoto <tom@duckyscan.com>
2024-01-19 22:44:57 +0700

    add walletconnect verify

--- components/Ecosystem.tsx
+++ components/Ecosystem.tsx
@@ -1,15 +1,21 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image';
+import JibCard from './JibCard';
 
-import JibCard from './JibCard'
+import iconJibid from '../public/img/icon/ecosystem/jibid.png';
+import iconJiblend from '../public/img/icon/ecosystem/jiblend.png';
+import iconJibpunk from '../public/img/icon/ecosystem/jibpunk.png';
+import iconCommudao from '../public/img/icon/ecosystem/commudao.png';
+import iconMeawneon from '../public/img/icon/ecosystem/meawneon.png';
+import iconJibswap from '../public/img/icon/ecosystem/jibswap.png';
 
-import iconJibid from '../public/img/icon/ecosystem/jibid.png'
-import iconJiblend from '../public/img/icon/ecosystem/jiblend.png'
-import iconJibpunk from '../public/img/icon/ecosystem/jibpunk.png'
-import iconCommudao from '../public/img/icon/ecosystem/commudao.png'
-import iconMeawneon from '../public/img/icon/ecosystem/meawneon.png'
-import iconJibswap from '../public/img/icon/ecosystem/jibswap.png'
+interface EcosystemItem {
+  icon: StaticImageData;
+  name: string;
+  tag: string[];
+  description: string;
+}
 
-const tiers = [
+const items: EcosystemItem[] = [
   {
     icon: iconJibswap,
     name: 'JibSwap',
@@ -43,15 +49,13 @@ const tiers = [
   {
     icon: iconJiblend,
     name: 'JIBLEND',
-    tag: ['DEFI',],
+    tag: ['DEFI'],
     description: 'Unleash the Power of Your Crypto',
   },
   
-]
-
+];
 
 export default function Ecosystem() {
-  
   return (
     <div className="py-24 sm:py-32" id="ecosystem">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -61,33 +65,35 @@ export default function Ecosystem() {
           </p>
         </div>
         <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-secondary-100 pb-12">
-        The number of DApps on our platform is growing at breakneck speed, attracting users and pushing 
-        the boundaries of what's possible. Join the revolution and be part of the future.
+          The number of DApps on our platform is growing at breakneck speed, attracting users and pushing 
+          the boundaries of what's possible. Join the revolution and be part of the future.
         </p>
 
         <div className="mx-auto max-w-6xl">
-          <div className="grid grid-cols-3 gap-8">
-            {tiers.map((tier) => (
-              <JibCard key={tier.name}>
-                <div className="grid grid-cols-1 justify-items-start">
-                  <span className='pl-3'>
-                  <Image
-                    src={tier.icon}
-                    width={72}
-                    height={72}
-                    alt={tier.name}
-                  />
-                  </span>
-                  <div className='text-lg font-semi-bold p-3'>
-                    <span className='text-white text-2xl'>{tier.name}</span>
-                    <div>
-                      {tier.tag.map((tag) => (
-                        <span className="inline-flex mr-3 my-3 bg-secondary-200 items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-secondary-100 ">
-                          {tag}
-                        </span>
-                      ))}
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-8">
+            {items.map((item, index) => (
+              <JibCard key={index}>
+                <div>
+                  <div className="grid grid-cols-1 justify-items-start">
+                    <span className='pl-3'>
+                      <Image
+                        src={item.icon}
+                        width={72}
+                        height={72}
+                        alt={item.name}
+                      />
+                    </span>
+                    <div className='text-lg font-semi-bold p-3'>
+                      <span className='text-white text-2xl'>{item.name}</span>
+                      <div>
+                        {item.tag.map((tag, tagIndex) => (
+                          <span key={tagIndex} className="inline-flex mr-3 my-3 bg-secondary-200 items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-secondary-100">
+                            {tag}
+                          </span>
+                        ))}
+                      </div>
+                      <p className='text-secondary-100'>{item.description}</p>
                     </div>
-                    <p className='text-secondary-100'>{tier.description}</p>
                   </div>
                 </div>
               </JibCard>
@@ -96,5 +102,5 @@ export default function Ecosystem() {
         </div>
       </div>
     </div>
-  )
+  );
 }
