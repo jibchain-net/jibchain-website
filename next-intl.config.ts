2e8dfcf09b4b8f729318e80ad0d2a24b5aa065bd
Satoshi Okamoto <tony@duckyscan.com>
2025-04-10 15:21:03 +0700

    Refactor: Update `getrequestConfig` for next-intl 3.26.5
    This commit updates the `getrequestConfig` function to align with next-intl version 3.26.5. It simplifies the implementation and explicitly returns the locale to satisfy current API requirements. Also includes minor change to tsconfig.json.

--- next-intl.config.ts
+++ next-intl.config.ts
@@ -2,17 +2,13 @@ import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { locales } from './app/i18n-config';
 
-// Following exactly: https://next-intl.dev/blog/next-intl-3-22#await-request-locale
-export default getRequestConfig(async (context) => {
-  // Function parameters will be renamed in the next major version
-  // from `locale` to `requestLocale`
-  const locale = context.locale;
-
+// Simpler implementation for next-intl 3.26.5
+export default getRequestConfig(async ({ locale }) => {
   return {
     messages: (await import(`./messages/${locale}.json`)).default,
-    // We need to return the locale to satisfy the error message
-    // "A locale is expected to be returned from getRequestConfig"
-    locale: locale
+    // We explicitly return locale to satisfy the current API requirements
+    // This may need to be updated in future versions
+    locale
   };
 });
 
