5de875593f1229da89c78a8dbb6c0cd8b5768127
Satoshi Okamoto <tony@duckyscan.com>
2025-04-10 15:15:54 +0700

    Fix: Adapt to next-intl v3.2+ and export navigation helpers
    This commit adapts the `next-intl.config.ts` file to be compatible with next-intl v3.2+. It updates the `getRequestConfig` function to handle the locale directly and returns the locale. It also exports shared pathname navigation helpers for reuse in the app.

--- next-intl.config.ts
+++ next-intl.config.ts
@@ -1,11 +1,20 @@
+import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 import { getRequestConfig } from 'next-intl/server';
+import { locales } from './app/i18n-config';
 
+// Following exactly: https://next-intl.dev/blog/next-intl-3-22#await-request-locale
 export default getRequestConfig(async (context) => {
-  // In next-intl 3.22+, requestLocale is already a string, not a function
+  // Function parameters will be renamed in the next major version
+  // from `locale` to `requestLocale`
   const locale = context.locale;
-  
+
   return {
     messages: (await import(`./messages/${locale}.json`)).default,
-    locale
+    // We need to return the locale to satisfy the error message
+    // "A locale is expected to be returned from getRequestConfig"
+    locale: locale
   };
-}); 
+});
+
+// Export createSharedPathnamesNavigation for reuse in the app
+export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({ locales }); 
